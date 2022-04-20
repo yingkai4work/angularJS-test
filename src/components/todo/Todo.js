d01b5c969eab3c4dcc9734e175a4cfa0052c86e3
seyseysey <zhouyuheng_sey@outlook.com>
2022-04-20 10:27:47 +0800

    [zyh]feat: add finishedList and render two different lists

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -5,36 +5,8 @@ angular.module('myApp.view1').component('todo', {
     template: html,
     bindings: {
         taskList: '=',
+        finishedList: '=',
     },
-    controller: function TodoController($scope) {
-        this.test = [{
-            id: 0,
-            name: 'task 01',
-            completed: false,
-        }, {
-            id: 1,
-            name: 'task 02',
-            completed: false,
-        }, {
-            id: 2,
-            name: 'task 03',
-            completed: false,
-        }, {
-            id: 3,
-            name: 'task 04',
-            completed: true,
-        }, {
-            id: 4,
-            name: 'task 05',
-            completed: true,
-        },
-        ];
-        // this.testNum = 1;
-        // this.completed = completed.filter($scope.taskList, true);
-        console.log(this);
-        console.log(this.test);
-        console.log(this.taskList);
-        console.log($scope);
-        console.log($scope.$ctrl.taskList);
-    }
-})
+    controller: function TodoController() {
+
+    }});
--- src/pages/view1/view1.js
+++ src/pages/view1/view1.js
@@ -8,28 +8,29 @@ angular.module("myApp.view1", ["ngRoute"])
             controller: 'View1Ctrl'
         });
     }]).controller('View1Ctrl', ['$scope', function ($scope) {
-        console.log("111111")
-        $scope.taskList = [{
-            id: 0,
-            name: 'task 01',
-            completed: false,
-        }, {
-            id: 1,
-            name: 'task 02',
-            completed: false,
-        }, {
-            id: 2,
-            name: 'task 03',
-            completed: false,
-        }, {
-            id: 3,
-            name: 'task 04',
-            completed: true,
-        }, {
-            id: 4,
-            name: 'task 05',
-            completed: true,
-        },
-        ];
-    }]);
+    console.log("111111")
+    $scope.taskList = [{
+        id: 0,
+        name: 'task 01',
+        completed: false,
+    }, {
+        id: 1,
+        name: 'task 02',
+        completed: false,
+    }, {
+        id: 2,
+        name: 'task 03',
+        completed: false,
+    },
+    ];
+    $scope.finishedList = [{
+        id: 3,
+        name: 'task 04',
+        completed: true,
+    }, {
+        id: 4,
+        name: 'task 05',
+        completed: true,
+    },];
+}]);
 import "../../components/todo/Todo"
